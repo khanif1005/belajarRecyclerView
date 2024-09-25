f67e4de2adb5cc17dbfa6e5720f1a5101c0886af
khanif1005 <abieddafiq10@gmail.com>
2024-09-25 19:33:41 +0700

    belajar recyclerView

--- app/src/main/java/com/latihan/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/latihan/myrecyclerview/ListHeroAdapter.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.AdapterListUpdateCallback
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
@@ -23,11 +25,17 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+        // Menggunakan Glide untuk memuat gambar
+        Glide.with(holder.itemView.context)
+            .load(photo) // URL Gambar
+            .into(holder.imgPhoto) // imageView mana yang akan diterapkan
+
         holder.tvName.text = name
         holder.tvDescription.text = description
         holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
             Toast.makeText(holder.itemView.context, "kamu memilih " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
         }
 
